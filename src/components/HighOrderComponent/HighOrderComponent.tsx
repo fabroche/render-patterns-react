2668f6d9c37c5c53235592056dc196e266f157de
fabroche <kraxusmmo@gmail.com>
2024-12-02 17:45:26 +0000

    feat: High Order Component withLoading

--- src/components/HighOrderComponent/HighOrderComponent.tsx
+++ src/components/HighOrderComponent/HighOrderComponent.tsx
@@ -1,6 +1,25 @@
+function withLoading<T extends object>(Component: React.ComponentType<T>) {
+    return (props: T & { isLoading: boolean }) => {
+        const {isLoading, ...rest} = props
+        return isLoading ? <p>Loading...</p> : <Component {...rest as T}/>
+    }
+}
+
+interface UserProps {
+    name: string,
+}
+
+const UserComponent: React.FC<UserProps> = ({name}) => {
+ return <p>Hello, {name}!</p>
+}
+
+const UserWithLoading = withLoading(UserComponent)
+
 export const ParentComponent = () => {
-  return (
-    <div>
-    </div>
-  );
+    return (
+        <div>
+            <UserWithLoading isLoading={true} name="fabroche"/>
+            <UserWithLoading isLoading={false} name="fabroche"/>
+        </div>
+    );
 };
